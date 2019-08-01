9f3ddc369eb7149c6c7e61f98df4c1a7c5c395aa
Doug Banville <doug.banville@rte.ie>
2019-08-01 17:32:03 +0100

    some actions

--- addon/components/ember-peaks.js
+++ addon/components/ember-peaks.js
@@ -18,7 +18,7 @@ export default Component.extend({
     const options = {
       container: document.querySelector("#peaks-container"),
       mediaElement: document.querySelector("audio"),
-      //zoomLevels: [512, 1024, 2048, 4096],
+      zoomLevels: [512, 1024, 2048, 4096],
       height: 400,
       dataUri: {
         json: this.json
@@ -37,8 +37,51 @@ export default Component.extend({
 
   actions: {
     play() {
-      alert("ply");
       this.instance.player.play();
+    },
+    zoomOut() {
+      let view = this.instance.views.getView("zoomview");
+      this.instance.zoom.zoomOut();
+    },
+    zoomINt() {
+      let view = this.instance.views.getView("zoomview");
+      this.instance.zoom.zoomIn();
+    },
+    segment(id) {
+      this.instance.segments.add({
+        startTime: 0,
+        endTime: 1000.5,
+        label: "0 to 300 seconds",
+        color: "#666",
+        id: id
+      });
+    },
+    removeSegment(id) {
+      this.instance.segments.removeById(id);
+    },
+    addPoint(id = "doug") {
+      this.instance.points.add({
+        time: 3.5,
+        labelText: "Test point",
+        color: "#666",
+        id: id
+      });
+    },
+    removePoint(id) {
+      this.instance.points.removeById(id);
+    },
+    markStart() {
+      //console.log(this.instance.player.getCurrentTime());
+      this.set("segmentStart", this.instance.player.getCurrentTime());
+    },
+    markEnd() {
+      this.set("segmentEnd", this.instance.player.getCurrentTime());
+      this.instance.segments.add({
+        startTime: this.segmentStart,
+        endTime: this.segmentEnd,
+        label: "0 to 300 seconds",
+        color: "#666"
+      });
     }
   }
 });
